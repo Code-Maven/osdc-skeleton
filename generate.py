0f07e76435eb45163522702d7261363cd6179a0d
Gabor Szabo <gabor@szabgab.com>
2023-01-07 22:14:45 +0200

    generate page

--- generate.py
+++ generate.py
@@ -5,8 +5,18 @@ import os
 def main():
     if not os.path.exists("_site"):
         os.makedirs("_site")
+
+
+    mentors = read_json_files('mentors')
     with open("_site/index.html", "w") as fh:
-        fh.write("OSDC Skeleton")
+        fh.write("<h1>OSDC Skeleton</h1>\n")
+        fh.write("<h2>Mentors</h2>\n")
+        fh.write("<ul>\n")
+        for mentor in mentors:
+            fh.write(f"<li>{mentor['name']}<br>\n")
+            fh.write(f'''GitHub: <a href="https://github.com/{mentor['github']}">{mentor['github']}</a><br>\n''')
+            fh.write(f"</li>\n")
+        fh.write("</ul>\n")
 
 def read_json_files(folder):
     people = []
