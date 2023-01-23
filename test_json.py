c208a06a64f2d54feff8e96753c1f0426051aba5
Gabor Szabo <gabor@szabgab.com>
2023-01-23 13:40:28 +0200

    remove old code that is already in the osdc-site-generator repo

--- test_json.py
+++
@@ -1,9 +0,0 @@
-from generate import read_json_files
-
-def test_json():
-    for folder in ['participants', 'mentors']:
-        people = read_json_files(folder)
-        for person in people:
-            assert 'name' in person
-
-
